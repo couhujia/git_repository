8f42c731981e1874a0f74b4b50d5fd4a7f6cc55d
couhujia <couhujia@qq.com>
2017-03-07 17:36:05 +0800

    新增项目

---
+++ Test/SpringRESTfulWebService/src/main/java/hope/SpringResTfulWebServiceApplication.java
@@ -0,0 +1,12 @@
+package hope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringResTfulWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringResTfulWebServiceApplication.class, args);
+	}
+}
