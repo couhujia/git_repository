f3613aadeadeb583222370a36b0ea83ed4123680
couhujia <couhujia@qq.com>
2017-04-20 12:02:37 +0800

    增加了redis缓存

--- platform/src/main/java/hope/server/web/RoleController.java
+++ platform/src/main/java/hope/server/web/RoleController.java
@@ -16,41 +16,46 @@ import hope.server.service.RoleService;
 
 @RestController
 public class RoleController {
-	
+
 	private final RoleService roleService;
-	protected static Logger logger=LoggerFactory.getLogger(RoleController.class);
-	
+	protected static Logger logger = LoggerFactory.getLogger(RoleController.class);
+
 	@Autowired
-	public RoleController(RoleService roleService){
-		this.roleService=roleService;
+	public RoleController(RoleService roleService) {
+		this.roleService = roleService;
 	}
-	
+
 	@RequestMapping("/role/findAll")
-	//@PreAuthorize("hasRole('superadmin')")
-	public Collection<Role> FindAll(){
+	// @PreAuthorize("hasRole('superadmin')")
+	public Collection<Role> FindAll() {
 		logger.info("in the find All");
 		return this.roleService.FindAll();
 	}
-	
+
 	@RequestMapping("/role/findByName")
-	public Optional<Role> FindByName(@RequestParam String name){
+	public Optional<Role> FindByName(@RequestParam String name) {
 		return this.roleService.FindByName(name);
 	}
-	
+
 	@RequestMapping("/role/findById")
-	public Optional<Role> FindById(@RequestParam String id){
-		return this.roleService.FindByName(id);
+	public Role FindById(@RequestParam String id) {
+		Optional<Role> role = this.roleService.FindById(Long.parseLong(id));
+		if (role.isPresent()) {
+			return role.get();
+		} else {
+			return null;
+		}
 	}
-	
+
 	@RequestMapping("/role/save")
-	//@PreAuthorize("hasRole('superadmin')")
-	public Optional<Role> Save(@RequestParam String name){
+	// @PreAuthorize("hasRole('superadmin')")
+	public Optional<Role> Save(@RequestParam String name) {
 		return this.roleService.Save(name);
 	}
-	
+
 	@RequestMapping("/role/deleteByName")
-	//@PreAuthorize("hasRole('superadmin')")
-	public String Delete(@RequestParam String name){
+	// @PreAuthorize("hasRole('superadmin')")
+	public String Delete(@RequestParam String name) {
 		this.roleService.DeleteByName(name);
 		return "delete Successful";
 	}
